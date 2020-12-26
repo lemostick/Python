b38f500f41c5b759a40e6bddb26c75ee0c8845d6
lemostick <lemostick@gmail.com>
2020-12-26 18:11:28 +0300

    Выполнил задание ко 2 уроку.

---
+++ 1.py
@@ -0,0 +1,6 @@
+list = [192, 'String', 17.5, True]
+print ('Первый элемент списка - ' + str(type(list[0])))
+print ('Второй элемент списка - ' + str(type(list[1])))
+print ('Третий элемент списка - ' + str(type(list[2])))
+print ('Четвертый элемент списка - ' + str(type(list[3])))
+print ("       Справка: \nint - целое число\nstr - строка (текст)\nfloat - число с дробной частью\nbool - булево значение (True/False")
